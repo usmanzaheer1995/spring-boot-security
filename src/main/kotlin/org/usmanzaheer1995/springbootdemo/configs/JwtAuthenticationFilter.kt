0a03865a616ce0648cdc931922e0bb708b7ba4b4
Usman Zaheer <usmanzaheer1995@gmail.com>
2024-04-21 22:29:35 +0200

    add security configuration

---
+++ src/main/kotlin/org/usmanzaheer1995/springbootdemo/configs/JwtAuthenticationFilter.kt
@@ -0,0 +1,63 @@
+package org.usmanzaheer1995.springbootdemo.configs
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import org.usmanzaheer1995.springbootdemo.services.CustomUserDetailsService
+import org.usmanzaheer1995.springbootdemo.services.TokenService
+
+@Component
+class JwtAuthenticationFilter(
+    private val userDetailsService: CustomUserDetailsService,
+    private val tokenService: TokenService,
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain,
+    ) {
+        val authHeader: String? = request.getHeader("Authorization")
+
+        if (authHeader.doesNotContainerBearerToken()) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val jwtToken = authHeader!!.extractTokenValue()
+        val email = tokenService.extractEmail(jwtToken)
+
+        if (email != null && SecurityContextHolder.getContext().authentication == null) {
+            val foundUser = userDetailsService.loadUserByUsername(email)
+
+            if (tokenService.isValid(jwtToken, foundUser)) {
+                updateContext(foundUser, request)
+            }
+
+            filterChain.doFilter(request, response)
+        }
+    }
+
+    private fun updateContext(
+        foundUser: UserDetails,
+        request: HttpServletRequest,
+    ) {
+        val authToken = UsernamePasswordAuthenticationToken(foundUser, null, foundUser.authorities)
+        authToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+
+        SecurityContextHolder.getContext().authentication = authToken
+    }
+
+    private fun String?.doesNotContainerBearerToken(): Boolean {
+        return this == null || !this.startsWith("Bearer ")
+    }
+
+    private fun String.extractTokenValue(): String {
+        return this.substringAfter("Bearer ")
+    }
+}
